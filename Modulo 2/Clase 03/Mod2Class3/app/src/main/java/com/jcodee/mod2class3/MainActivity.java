91e9f9a59eb246285e58e211cde9f1eaaad4b330
Johann Jara <johannfjs@gmail.com>
2017-07-14 22:03:13 -0500

    clase 04

--- Modulo 2/Clase 03/Mod2Class3/app/src/main/java/com/jcodee/mod2class3/MainActivity.java
+++ Modulo 2/Clase 03/Mod2Class3/app/src/main/java/com/jcodee/mod2class3/MainActivity.java
@@ -3,6 +3,8 @@ package com.jcodee.mod2class3;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,6 +14,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.jcodee.mod2class3.fragments.AgregarCienteFragment;
+import com.jcodee.mod2class3.fragments.AgregarMarcaFragment;
+import com.jcodee.mod2class3.fragments.AgregarModeloFragment;
+import com.jcodee.mod2class3.fragments.VerClientesFragment;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -71,14 +78,33 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
+        Fragment fragment = null;
+        String titulo = "";
+
         if (id == R.id.nav_agregar_cliente) {
+            fragment = new AgregarCienteFragment();
+            titulo = "Agregar Cliente";
+        } else if (id == R.id.nav_agregar_marca) {
+            fragment = new AgregarMarcaFragment();
+            titulo = "Agregar Marca";
+        } else if (id == R.id.nav_agregar_modelo) {
+            fragment = new AgregarModeloFragment();
+            titulo = "Agregar Modelo";
+        } else if (id == R.id.nav_ver_clientes) {
+            fragment = new VerClientesFragment();
+            titulo = "Ver Clientes";
+        }
 
-        }else if(id==R.id.nav_agregar_marca){
-
-        }else if(id==R.id.nav_agregar_modelo){
-
-        }else if(id==R.id.nav_ver_clientes){
-
+        //Validamos que el fragmento no esté vacío
+        if (fragment != null) {
+            //Obtenemos la configuración del fragment
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            //Reemplazamos el componente por el fragmento
+            fragmentManager.beginTransaction()
+                    .replace(R.id.contenedor, fragment)
+                    .commit();
+            //Cambiamos el titulo a mostrar
+            setTitle(titulo);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
---
+++ Modulo 2/Clase 03/Mod2Class3/app/src/main/java/com/jcodee/mod2class3/database/MetodoSQL.java
@@ -0,0 +1,49 @@
+package com.jcodee.mod2class3.database;
+
+import com.jcodee.mod2class3.entidades.MarcaEntidad;
+import com.jcodee.mod2class3.entidades.ModeloEntidad;
+
+import io.realm.Realm;
+import io.realm.RealmModel;
+import io.realm.RealmResults;
+
+/**
+ * Created by johannfjs on 14/07/17.
+ * Email: johann.jara@jcodee.com
+ * Phone: (+51) 990 870 011
+ */
+
+public class MetodoSQL {
+    public void agregarMarca(MarcaEntidad entidad) {
+        //Obtenemos la configuración de la base de datos
+        Realm realm = Realm.getDefaultInstance();
+        //Abrimos una transacción nueva
+        realm.beginTransaction();
+        //Insertamos el registro a nuestra base de datos
+        realm.copyToRealm(entidad);
+        //Se guarda el registro en la base de datos
+        realm.commitTransaction();
+    }
+
+    public long obtenerIdMarca() {
+        Realm realm = Realm.getDefaultInstance();
+        return realm.where(MarcaEntidad.class).count() + 1;
+    }
+
+    public void agregar(Object object) {
+        Realm realm = Realm.getDefaultInstance();
+        realm.beginTransaction();
+        realm.copyToRealm((RealmModel) object);
+        realm.commitTransaction();
+    }
+
+    public RealmResults<MarcaEntidad> obtenerMarcas() {
+        Realm realm = Realm.getDefaultInstance();
+        return realm.where(MarcaEntidad.class).findAll();
+    }
+
+    public long obtenerIdModelo() {
+        Realm realm = Realm.getDefaultInstance();
+        return realm.where(ModeloEntidad.class).count() + 1;
+    }
+}
